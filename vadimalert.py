2db2f661725acf445a669ed6d63be9ec649ec63c
pythonsnek420 <56219300+pythonsnek420@users.noreply.github.com>
2019-10-06 13:05:13 +0300

    Revert "Add files" (#2)

--- vadimalert.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Mike'
-
-print("This is the vadim alert. This is only for emergencies. Please type 'VADIM' to confirm")
-confirmation = input()
-if confirmation == ("VADIM"):
-    while 3 > 2:
-        print("Vadim blin")
-        
